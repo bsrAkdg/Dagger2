7da5af8f5221f8944eb821e9a3d06576d37639f8
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-30 00:09:18 +0300

    Method injection

--- BeginnerDagger2/app/src/main/java/com/bsrakdg/beginnerdagger2/Car.java
+++ BeginnerDagger2/app/src/main/java/com/bsrakdg/beginnerdagger2/Car.java
@@ -5,18 +5,23 @@ import android.util.Log;
 import javax.inject.Inject;
 
 public class Car {
+    // The order : Constructor injection -> Field injection -> Method injection
     private static final String TAG = "Car";
 
-    private Engine engine;
+    @Inject Engine engine; // Field injection
     private Wheels wheels;
 
     @Inject
-    public Car(Engine engine, Wheels wheels) {
-        this.engine = engine;
+    public Car(Wheels wheels) { // Constructor injection
         this.wheels = wheels;
     }
 
     public void drive() {
         Log.d(TAG, "drive: driving...");
+    }
+
+    @Inject
+    public void enableRemote(Remote remote) { // Method injection
+        remote.setListener(this);
     }
 }
