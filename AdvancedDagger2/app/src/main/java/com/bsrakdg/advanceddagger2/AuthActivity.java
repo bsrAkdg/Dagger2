7ef458bf17e58bb986ec907e76ed4e25c8b3aad4
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-02 17:16:22 +0300

    Injecting Activities and @ContributesAndroidInjector

---
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/AuthActivity.java
@@ -0,0 +1,24 @@
+package com.bsrakdg.advanceddagger2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class AuthActivity extends DaggerAppCompatActivity {
+
+    private static final String TAG = "AuthActivity";
+
+    @Inject
+    String testString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        Log.d(TAG, "onCreate: " + testString);
+    }
+}
---
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/ActivityBuildersModule.java
@@ -0,0 +1,20 @@
+package com.bsrakdg.advanceddagger2.di;
+
+import com.bsrakdg.advanceddagger2.AuthActivity;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector // needed abstract
+    abstract AuthActivity contributeAuthActivity(); // AuthActivity wait data from AppComponent
+
+    @Provides
+    static String someString() {
+        return "Test string";
+    }
+
+}
