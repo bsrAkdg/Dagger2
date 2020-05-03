1cc54a894f40a97c9910265e3bf5649910c5c695
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-03 23:33:10 +0300

    LiveData reactive streams and flowables

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/auth/AuthActivity.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/auth/AuthActivity.java
@@ -2,11 +2,17 @@ package com.bsrakdg.advanceddagger2.ui.auth;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bsrakdg.advanceddagger2.R;
+import com.bsrakdg.advanceddagger2.models.User;
 import com.bsrakdg.advanceddagger2.viewmodels.ViewModelProviderFactory;
 import com.bumptech.glide.RequestManager;
 
@@ -14,7 +20,7 @@ import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
 
@@ -28,15 +34,50 @@ public class AuthActivity extends DaggerAppCompatActivity {
     RequestManager requestManager;
 
     private AuthViewModel viewModel;
+    private EditText userId;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button:
+                attemtLogin();
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        userId = findViewById(R.id.user_id_input);
+        findViewById(R.id.login_button).setOnClickListener(this);
+
         viewModel = new ViewModelProvider(this, providerFactory).get(AuthViewModel.class);
 
         setLogo();
+
+        subscribeObservers();
+    }
+
+    private void attemtLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())) {
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+    }
+
+    private void subscribeObservers() {
+        viewModel.observableUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user != null) {
+                    Log.d(TAG, "onChanged: " + user.getEmail());
+                } else {
+                    Log.e(TAG, "onChanged: USER IS NULL" );
+                }
+            }
+        });
     }
 
     private void setLogo() {
