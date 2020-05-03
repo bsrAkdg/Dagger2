e455389eb8120b8f2b01da77462d5d460b788290
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-03 14:19:47 +0300

    @Singleton scope

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/AppComponent.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/AppComponent.java
@@ -4,11 +4,16 @@ import android.app.Application;
 
 import com.bsrakdg.advanceddagger2.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+// AppComponent owns the @Singleton scope
+// if AppComponent dies all dependencies die
+@Singleton
 @Component(modules = {AndroidSupportInjectionModule.class,
         ActivityBuildersModule.class,
         AppModule.class})
--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/AppModule.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/AppModule.java
@@ -10,17 +10,21 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application) {
         return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application,
                                                RequestOptions requestOptions) {
@@ -29,6 +33,7 @@ public class AppModule {
 
     }
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions() {
         return RequestOptions
