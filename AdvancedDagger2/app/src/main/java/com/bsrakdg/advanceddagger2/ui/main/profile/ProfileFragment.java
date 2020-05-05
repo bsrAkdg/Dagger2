9cb894979f104e9921ea40876ddb16e7e1e8cfe9
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-06 02:01:22 +0300

    dagger2 fragments and viewmodels removeObservers(getViewLifecycleOwner())

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/profile/ProfileFragment.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/profile/ProfileFragment.java
@@ -5,14 +5,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bsrakdg.advanceddagger2.R;
+import com.bsrakdg.advanceddagger2.models.User;
+import com.bsrakdg.advanceddagger2.ui.auth.AuthResource;
 import com.bsrakdg.advanceddagger2.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -27,12 +30,12 @@ public class ProfileFragment extends DaggerFragment {
     @Inject
     ViewModelProviderFactory providerFactory;
 
+    private TextView email, username, website;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "Profile fragment create", Toast.LENGTH_LONG).show();
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
@@ -40,6 +43,52 @@ public class ProfileFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: Profile fragment was created");
 
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+
         profileViewModel = new ViewModelProvider(this, providerFactory).get(ProfileViewModel.class);
+
+        // DON'T DO THIS !!!!!
+        // Every time view created you receive an instance
+        // Best way receive session manager you should use subscribe own lifecycle owner :
+        // subscribeObservers()
+        /*AuthResource<User> user = ((MainActivity) getActivity()).sessionManager.getAuthUser()
+                .getValue();*/
+
+        subscribeObservers();
+    }
+
+    private void setErrorDetails(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private void setUserDetails(User data) {
+        username.setText(data.getUsername());
+        email.setText(data.getEmail());
+        website.setText(data.getWebsite());
+    }
+
+    private void subscribeObservers() {
+        // When Fragments replaced or destroyed, old observers are not deleted by activity, you should remove
+        profileViewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+
+        profileViewModel.getAuthenticatedUser()
+                .observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+                    @Override
+                    public void onChanged(AuthResource<User> userAuthResource) {
+                        if (userAuthResource != null) {
+                            switch (userAuthResource.status) {
+                                case AUTHENTICATED:
+                                    setUserDetails(userAuthResource.data);
+                                    break;
+                                case ERROR:
+                                    setErrorDetails(userAuthResource.message);
+                                    break;
+                            }
+                        }
+                    }
+                });
+
     }
 }
--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/profile/ProfileViewModel.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/profile/ProfileViewModel.java
@@ -2,16 +2,27 @@ package com.bsrakdg.advanceddagger2.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.bsrakdg.advanceddagger2.SessionManager;
+import com.bsrakdg.advanceddagger2.models.User;
+import com.bsrakdg.advanceddagger2.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
 public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
+    private final SessionManager sessionManager;
 
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: viewmodel is ready...");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser() {
+        return sessionManager.getAuthUser();
     }
 }
