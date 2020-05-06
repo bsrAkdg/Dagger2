0cb8abfeacc89be90160269a297ec4fd21dafc93
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-07 00:19:11 +0300

    Navigation drawer androidx

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/MainActivity.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/MainActivity.java
@@ -8,26 +8,28 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.bsrakdg.advanceddagger2.BaseActivity;
 import com.bsrakdg.advanceddagger2.R;
-import com.bsrakdg.advanceddagger2.ui.main.posts.PostsFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: starting");
-        testFragment();
-    }
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
-    private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostsFragment()).commit();
+        Log.d(TAG, "onCreate: starting");
     }
 
     @Override
@@ -45,5 +47,18 @@ public class MainActivity extends BaseActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                break;
+            case R.id.nav_posts:
+                break;
+        }
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
