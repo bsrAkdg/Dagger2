8e943398ad319aa69c2cdf35bc4b3cbdde9ff295
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-07 01:16:43 +0300

    Resolving problems with NavController and Navigation Graph

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/MainActivity.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/ui/main/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
@@ -44,25 +45,21 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.logout:
-                sessionManager.logOut();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_profile:
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true) // clear back stack
+
+                        .build();
                 Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.profileScreen);
+                        .navigate(R.id.profileScreen, null, navOptions);
                 break;
             case R.id.nav_posts:
-                Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.postsScreen);
+                if (isValidDestination(R.id.postsScreen)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment)
+                            .navigate(R.id.postsScreen);
+                }
                 break;
         }
         item.setChecked(true);
@@ -70,10 +67,40 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         return false;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                sessionManager.logOut();
+                return true;
+            case android.R.id.home: // for hamurger button click fix
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else {
+                    return false;
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation
+                .findNavController(this, R.id.nav_host_fragment), drawerLayout);
+    }
+
     private void init() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
         NavigationUI.setupWithNavController(navigationView, navController);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private boolean isValidDestination(int destination) {
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment)
+                .getCurrentDestination().getId();
     }
 }
