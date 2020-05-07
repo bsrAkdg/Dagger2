3c57b7fda2eaaf67540f864d1ffb7467a19af856
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-07 14:13:25 +0300

    Scoping subcomponents with dagger 2

--- AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/ActivityBuildersModule.java
+++ AdvancedDagger2/app/src/main/java/com/bsrakdg/advanceddagger2/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.bsrakdg.advanceddagger2.di;
 
 import com.bsrakdg.advanceddagger2.di.auth.AuthModule;
+import com.bsrakdg.advanceddagger2.di.auth.AuthScope;
 import com.bsrakdg.advanceddagger2.di.auth.AuthViewModelsModule;
 import com.bsrakdg.advanceddagger2.di.main.MainFragmentBuildersModule;
 import com.bsrakdg.advanceddagger2.di.main.MainModule;
+import com.bsrakdg.advanceddagger2.di.main.MainScope;
 import com.bsrakdg.advanceddagger2.di.main.MainViewModelsModule;
 import com.bsrakdg.advanceddagger2.ui.auth.AuthActivity;
 import com.bsrakdg.advanceddagger2.ui.main.MainActivity;
@@ -14,11 +16,13 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(modules = {
             AuthViewModelsModule.class,
             AuthModule.class})
     abstract AuthActivity contributeAuthActivity(); // AuthActivity wait data from AppComponent
 
+    @MainScope
     @ContributesAndroidInjector(modules = {
             MainFragmentBuildersModule.class,
             MainViewModelsModule.class,
